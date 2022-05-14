a6895a6079d83a9d8be4e23ace120def35374a7a
吴辉 <1972871083@qq.com>
2022-05-14 18:34:08 +0800

    refactor: rename file

---
+++ commit-msg.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+	echo "Missing argument (commit message). Did you try to run this manually?"
+	exit 1
+fi
+
+commitTitle="$(cat $1 | head -n1)"
+
+# ignore merge requests
+if echo "$commitTitle" | grep -qE "^Merge branch \'"; then
+	echo "Commit hook: ignoring branch merge"
+	exit 0
+fi
+
+if ! echo "$commitTitle" | grep -qE '^(feat|fix|docs|style|refactor|perf|test|chore|workflow|build|ci|release)(\(.+\))?: .{1,100}'; then
+	echo "Bad formatted logging message"
+	echo "Please see https://github.com/One2r/commit-msg-hook"
+	exit 1
+fi
